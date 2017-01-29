8817370d05b470f5d095660c7ed755eb0df33571
Aaron Holmes <aholmes@pureguru-software.com>
2017-01-29 00:52:43 -0500

    Trying stuff for heroku

---
+++ dist-server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var compression = require('compression');
+
+var port = process.env.port || 3000;
+var app = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
+});
