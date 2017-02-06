41f0156256c2ef4bac536047392d54b69663d132
Aaron Holmes <aholmes@pureguru-software.com>
2017-02-05 22:56:43 -0500

    Setting state for business

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React, { PropTypes } from 'react';
 import { BrowserRouter, Match, Miss } from 'react-router';
+import _ from 'lodash';
 import HeaderNav from './HeaderNav/HeaderNav';
 import Search from './Search/Search';
 import Routes from './Routes';
+
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addBusiness = this.addBusiness.bind(this);
+    this.state = {
+      businesses: {},
+      order: {}
+    }
+  }
+
+  addBusiness(business) {
+    const newBusiness = {
+      id: _.uniqueId('business-'),
+      name: business.name,
+      address: business.address
+    };
+    this.setState({
+      businesses: this.state.businesses.concat(newBusiness),
+    });
+  }
+
   render() {
     return (
         <BrowserRouter>
