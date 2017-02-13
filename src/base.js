19da719dba78bce7a33f7eb0dbcb559c67ff3cd9
Aaron Holmes <aholmes@pureguru-software.com>
2017-02-12 21:12:36 -0500

    removing webpack from prod

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL
+  apiKey: "AIzaSyCjRBFNI7xFc28i4nznrCrFnylvUVXrCA0",
+  authDomain: "yelp-clone-543af.firebaseapp.com",
+  databaseURL: "https://yelp-clone-543af.firebaseio.com"
 });
 
 export default base;
